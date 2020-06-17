3998e7e87f140abca5b303b590e789366d4edd7c
jaeseungjeong <56663654+jaeseungjeong@users.noreply.github.com>
2020-06-17 21:22:00 +0900

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -X DELETE 'http://localhost:1026/v2/entitiesIIJ_ECHONET-test-1'  -H 'fiware-service: openiot' -H 'fiware-servicepath: /' -d '
+curl -X DELETE 'http://localhost:1026/v2/entitiesIIJ_ECHONET-test-1'  -H 'fiware-service: openiot' -H 'fiware-servicepath: /'
